d95bad3e618f5b74db640239352616d4c9e72c93
Oliver Spring <oliver9589@gmail.com>
2017-09-17 02:50:44 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -4,10 +4,10 @@ class Spaceship extends React.Component {
   render() {
     return (
       <div className='spaceship'>
-        <h1>{props.name}</h1>
-        <p>{props.speed}</p>
-        <p>{props.hasRockets}</p>
-        <small>{props.colors}</small>
+        <h1>{this.props.name}</h1>
+        <p>{this.props.speed}</p>
+        <p>{this.props.hasRockets}</p>
+        <small>{this.props.colors}</small>
       </div>
     )
   }
