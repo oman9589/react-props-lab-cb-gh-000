54e5036a92a11716135e6981b4fd98275976d598
Oliver Spring <oliver9589@gmail.com>
2017-09-17 04:04:36 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import Spaceship from './components/Spaceship';
 
 Spaceship.defaultProps = {
   speed: 'slow',
-  hasRockets: false,
-  colors: ['black', 'red']
+  hasRockets: 'false',
+  colors: "['black', 'red']"
 }
-
-const test = ['black', 'red'];
 
 ReactDOM.render(
   <Spaceship name="Millennium Falcon"
