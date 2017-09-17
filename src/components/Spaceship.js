7bbb4ad8ab7be16f2a60973cc8b7d1fa0573242d
Oliver Spring <oliver9589@gmail.com>
2017-09-17 02:52:40 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -7,7 +7,9 @@ class Spaceship extends React.Component {
         <h1>{this.props.name}</h1>
         <p>{this.props.speed}</p>
         <p>{this.props.hasRockets}</p>
-        <small>{this.props.colors}</small>
+        <ul>
+          {this.props.map((color) => <li color />)}
+        </ul>
       </div>
     )
   }
