cdb71306f9542967ceaec3ba101f7d21a9276b77
Oliver Spring <oliver9589@gmail.com>
2017-09-17 04:15:15 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class Spaceship extends React.Component {
-  static defaultProps = {
+  defaultProps = {
     speed: 'slow',
     hasRockets: false,
     colors: ['black', 'red'],
